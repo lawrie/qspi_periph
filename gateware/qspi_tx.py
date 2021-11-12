f89e809a7212b38b538483bc29638b68723b65db
lawrie <lawrie.griffiths@ntlword.com>
2021-11-12 11:22:41 +0000

    Support dial spi

--- gateware/qspi_tx.py
+++ gateware/qspi_tx.py
@@ -4,14 +4,15 @@ from nmigen.utils import bits_for
 from nmigen.hdl.ast import Rose, Fell
 
 class QspiTx(Elaboratable):
-    def __init__(self, pkt_size=16):
+    def __init__(self, pkt_size=16, qw=4):
         # Parameters
         self.pkt_size = pkt_size
+        self.qw       = qw
 
         # QSPI pins
         self.csn  = Signal()
         self.sclk = Signal()
-        self.qd   = Signal(4)
+        self.qd   = Signal(qw)
 
         # Inputs
         self.pkt   = Signal(self.pkt_size * 8)
@@ -22,23 +23,23 @@ class QspiTx(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
-        nibbles = Signal(bits_for(self.pkt_size) * 2)
+        chunks = Signal(bits_for(self.pkt_size) * (8 // self.qw))
         shift_reg = Signal(self.pkt_size * 8)
 
         with m.If(self.csn):
             m.d.sync += [
-                nibbles.eq(0),
+                chunks.eq(0),
                 shift_reg.eq(self.pkt)
             ]
         with m.Else():
             with m.If(Fell(self.sclk)):
                 m.d.sync += [
-                    nibbles.eq(nibbles+1),
-                    shift_reg.eq(Cat(C(0,4), shift_reg[:-4]))
+                    chunks.eq(chunks+1),
+                    shift_reg.eq(Cat(C(0,self.qw), shift_reg[:-self.qw]))
                 ]
 
-        m.d.comb += self.qd.eq(shift_reg[-4:]),
-        m.d.comb += self.ready.eq(nibbles == self.pkt_size * 2)
+        m.d.comb += self.qd.eq(shift_reg[-self.qw:]),
+        m.d.comb += self.ready.eq(chunks == self.pkt_size * (8 // self.qw))
 
         return m
 
