a49924f7c57768f8f38c6d6603cc18800c1d33c6
lawrie <lawrie.griffiths@ntlword.com>
2021-11-23 20:19:24 +0000

    Added more comments

--- gateware/bram_periph.py
+++ gateware/bram_periph.py
@@ -22,11 +22,13 @@ class BramPeriph(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
+        # Create the memory and its ports
         mem = Memory(width=8, depth=self.depth)
         m.submodules.w = w = mem.write_port()
         m.submodules.r = r = mem.read_port()
 
-        i_nb  = Signal(4)
+        # Signals
+        nb    = Signal(4)
         addr  = Signal(15)
         i_pkt = Signal(self.pkt_size * 8)
         req   = Signal()
@@ -37,37 +39,42 @@ class BramPeriph(Elaboratable):
         with m.If(self.i_valid & self.o_ready):
             m.d.sync += [
                 i_pkt.eq(self.i_pkt),
+                # Get the 15-bit address
                 addr.eq(self.i_pkt.bit_select(Cat(C(0,3), self.i_nb) - 16, 15)),
+                # Get the request bit
                 req.eq(req_byte),
-                i_nb.eq(Mux(req_byte == 1, self.i_nb - 2, self.i_pkt[:8] + 1)),
-                self.o_nb.eq(self.i_pkt[:8]) # Only used for reads
+                # Add 1 to number of bytes for read requests to make detecting 
+                # all bytes read simpler
+                nb.eq(Mux(req_byte == 1, self.i_nb - 2, self.i_pkt[:8] + 1)),
+                # Output number of bytes is only relevant for read requests
+                self.o_nb.eq(self.i_pkt[:8]) 
             ]
         
         # We are ready when we have written all the bytes, and, for reads, the output has been acked
-        m.d.comb += self.o_ready.eq((i_nb == 0) & ~self.o_valid)
+        m.d.comb += self.o_ready.eq((nb == 0) & ~self.o_valid)
 
         # Connect to memory
         m.d.comb += [
-            w.en.eq((i_nb > 0) & (req == 1)),
-            w.data.eq(i_pkt.word_select(i_nb - 1, 8)),
+            w.en.eq((nb > 0) & (req == 1)),
+            w.data.eq(i_pkt.word_select(nb - 1, 8)),
             w.addr.eq(addr),
             r.addr.eq(addr)
         ]
 
         # Decrement number of byte each cycle
-        with m.If(i_nb > 0):
-            m.d.sync += i_nb.eq(i_nb - 1)
+        with m.If(nb > 0):
+            m.d.sync += nb.eq(nb - 1)
 
         # Increment address for all but first byte
-        with m.If(i_nb > 0):
+        with m.If(nb > 0):
             m.d.sync += addr.eq(addr+1)
 
         # For read requests, copy bytes to o_pkt
-        with m.If(req == 0 & (i_nb > 0)):
-            m.d.sync += self.o_pkt.word_select(self.pkt_size - 1 - self.o_nb + i_nb, 8).eq(r.data)
+        with m.If(req == 0 & (nb > 0)):
+            m.d.sync += self.o_pkt.word_select(self.pkt_size - 1 - self.o_nb + nb, 8).eq(r.data)
 
         # Set o_valid when reading last byte
-        with m.If((req == 0) & (i_nb == 1)):
+        with m.If((req == 0) & (nb == 1)):
             m.d.sync += self.o_valid.eq(1)
 
         # Unset o_valid when acked
