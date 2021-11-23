d9ef38a627c13d66c6f0a678acdce1ad240ffe0a
Lawrie Griffiths <lawrie.griffiths@ntlworld.com>
2021-11-23 18:44:15 +0000

    Write and read back BRAM

--- src/QSPIETest/QSPIETest.ino
+++ src/QSPIETest/QSPIETest.ino
@@ -22,6 +22,11 @@ static char rx_pkt[17];
 
 static char sev_pkt[] = {0x30, 0x00};
 
+static char bram_write[] = {0x40, 0x80, 0x00, 0x30, 0x31, 0x32, 0x33, 0x34,
+                            0x35, 0x36, 0x37, 0x38, 0x39, 0x40, 0x41};
+
+static char bram_read[] = {0x40, 0x00, 0x00, 0x04};
+
 static bool configured = false;
 
 static int cnt = 0;
@@ -85,7 +90,7 @@ void loop() {
       // Write to peripheral 0 or 2 o2 3
       // If ready to send, write the packet
       if (rx_pkt[0] == 0xF0 || rx_pkt[0] == 0xB0) { // B0 is temporary hack
-        if (cnt % 3 == 0) {
+        if (cnt % 5 == 0) {
           Serial.println("Writing event 0");
           if (!QSPI.write(tx_pkt, 16))
             Serial.println("QSPI.transmit failed");
@@ -93,15 +98,23 @@ void loop() {
           char t = tx_pkt[15];
           for(int i=15;i>1;i--) tx_pkt[i] = tx_pkt[i-1];
           tx_pkt[1] = t;
-        } else if (cnt % 2 == 1) {
+        } else if (cnt % 5 == 1) {
           Serial.println("Writing event 0");
           if (!QSPI.write(hello, 14))
             Serial.println("QSPI.transmit failed");
-        } else {
+        } else if (cnt % 5 == 2){
           Serial.println("Writing event 3");
           sev_pkt[1] = sev_val++;
           if (!QSPI.write(sev_pkt, 2))
             Serial.println("QSPI.transmit failed");          
+        } else if (cnt % 5 == 3) {
+          Serial.println("Writing event 4 (write)");
+          if (!QSPI.write(bram_write, 15))
+            Serial.println("QSPI.transmit failed");
+        } else {
+          Serial.println("Writing event 4 (read)");
+          if (!QSPI.write(bram_read, 4))
+            Serial.println("QSPI.transmit failed");
         }
       } else if (rx_pkt[0] != 0xFF && digitalRead(PIN_DIRECTION) == 1) {
         Serial.println("Switching to receive");
